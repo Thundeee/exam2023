d60e943412a4ee32375e25b1dfd1c9b7281fce3e
Thundeee <asbjornabel@gmail.com>
2023-04-14 15:08:40 +0200

    layout added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@mui/material";
+import Layout from './components/Layout';
 import logo from './Logo.svg';
 
 function App() {
   return (
 
     <ThemeProvider theme={theme}>
+          <Layout>
       <Routes>
         <Route index element={    <div className="App">
       <h1>Holidaze</h1>
       <img src={logo} alt="Holidaze Logo" />
 </div>} />
       </Routes>
+      </Layout>
   </ThemeProvider>
   );
 }
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,4 @@
 import { createTheme } from "@mui/material";
-import { Salmon, wheat } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
